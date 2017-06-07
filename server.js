b9e102522b871f8a5808af868ce9bb9706554713
Avinash <mauryaavinash95@gmail.com>
2017-06-07 21:59:24 +0530

    Initial commit of my webserver

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var app = express();
+
+hbs.registerPartials(__dirname + "/views/partials");
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+})
+app.set('view engine', 'hbs');
+app.use((request, response, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${request.method} : ${request.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (error) => {
+        if (error) {
+            console.log('Unable to write log to the server');
+        }
+    });
+    next();
+});
+
+// app.use((request, response) => {
+//     response.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + "/public"));
+
+app.get('/', (request, response) => {
+    response.render('home.hbs', {
+        pageTitle: 'Home Page of my website'
+    });
+});
+
+app.get('/about', (request, response) => {
+    response.render('about.hbs', {
+        pageTitle: 'About Page',
+    });
+})
+
+app.listen(3000, () => {
+    console.log("Server is up on port 3000");
+});
